b24debfaa9e267f0050ec867510939db95924766
Tyler Ruotolo <33192847+TR-Software@users.noreply.github.com>
2022-09-22 21:35:41 -0400

    update

--- tr.js
+++
@@ -1,4 +0,0 @@
-if(confirm("This website and all of my other projects are kept running through my personal income. \n I have never been paid for programming and I can't afford to keep all of my websites up and running consistently. \n If you would be so kind as to donate to help keep the entire TR network up and running, I would really appreciate it. \n Every donation helps no matter the amount.")){
-  window.open('thanks.html');
-  window.open('https://www.paypal.me/tylerruotolo');
-}
